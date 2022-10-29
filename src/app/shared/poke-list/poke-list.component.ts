379812ae1e74b1a0f8b479fb08d717bc220ba58b
FalcaoCamilla <millafalcao.acad@gmail.com>
2022-10-29 13:44:43 -0300

    filter poke-search

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -9,6 +9,7 @@ import { PokeAPIService } from 'src/app/services/poke-api.service';
 export class PokeListComponent implements OnInit {
 
   public getAllPokemons: any;
+  public setAllPokemons: any;
 
   constructor(private PokeApiService: PokeAPIService){
 
@@ -17,10 +18,30 @@ export class PokeListComponent implements OnInit {
   ngOnInit(): void {
     this.PokeApiService.apiListAllPokemons.subscribe(
       res => {
-        this.getAllPokemons = res.results;
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
       }
     );
     //Ao utilizar o get, não é necessário passar qualquer valor como parâmetro
   }
+
+
+  public getSearch(value: string){
+    const filter = this.setAllPokemons.filter(
+      (res: any) => {
+        return !res.name.indexOf(value.toLowerCase())
+      }
+    );
+    this.getAllPokemons = filter;
+  }
+  /*
+  A função getSearch, acionada pelo EventEmitter instanciado pela propriedade "emitSearc",
+  cria uma constante chamada "filter", que recebe o retorno da solicitação do PokeAPIService
+  chamado, neste component, de getAllPokemons de maneira filtrada, por meio do filter do js.
+  O filter funciona, então, verificando se o res.name do valor enviado como parâmetro no poke-search
+  existe de fato. Caso não haja nenhum elemento de index X, com valor semelhante ou igual ao que foi passado,
+  nada é exibido.
+  No entanto, se o
+  */
 
 }
--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -7,7 +7,21 @@ import { EventEmitter } from '@angular/core';
   styleUrls: ['./poke-search.component.scss']
 })
 export class PokeSearchComponent implements OnInit {
-  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter()
+  @Output() public emitSearch: EventEmitter<string> = new EventEmitter()
+  /*
+  O output indica que um valor de saída será declarado.
+  Neste acaso, um valor deverá sair do component poke-search e ser recebido pelo poke-list
+
+  De modo geral, a ordem de emissão de eventos no angular é:
+  Pai emite para filho (input)
+  Filho emite para pai (output)
+
+  A propriedadde "emitSearch", do tipo EventEmitter retornando uma string,
+  recebe uma função construtora do próprio EventEmitter.
+
+  O EventEmitter torna possível confirmar que um valor foi emitido com sucesso.
+  Através do método search, é possível perceber isto.
+  */
 
   constructor() { }
 
@@ -15,7 +29,17 @@ export class PokeSearchComponent implements OnInit {
   }
 
   public search(value: string){
-    console.log(value);
+    this.emitSearch.emit(value);
   }
+  /*
+  A função search recebe um value, passado no html do component.
+
+  A propriedade emitSearch é utilizada no momento em que a função search() é acionada.
+  Como essa propriedade recebe o construtor do EventEmitter, é possível que
+  um valor (nesse caso o mesmo passado pelo parâmetro) seja emitido através dela.
+
+  Tudo isso é feito para que no component "poke-list", os dados enviados pelo "poke-search"
+  sejam válidos e utilizados (ver poke-list.html)
+  */
 
 }
